6052a1f99602dbeb97abc014951735f0b6172446
arunkumarkalai1398 <57340179+arunkumarkalai1398@users.noreply.github.com>
2020-10-23 14:46:43 +0530

    Add files via upload

---
+++ build/precache-manifest.8d9a9024e302b93aee88608277202d84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23696e8eea07399d9eba675216c2a0a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "810e861d358f0fd49170",
+    "url": "/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "cf0aa032b64bfe8ecee7",
+    "url": "/static/css/main.da576801.chunk.css"
+  },
+  {
+    "revision": "810e861d358f0fd49170",
+    "url": "/static/js/2.c9ef951f.chunk.js"
+  },
+  {
+    "revision": "d56f7d70e49a582c3cb88e84fa36367e",
+    "url": "/static/js/2.c9ef951f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf0aa032b64bfe8ecee7",
+    "url": "/static/js/main.0b1a0787.chunk.js"
+  },
+  {
+    "revision": "629a5ae524a642ef6fbe",
+    "url": "/static/js/runtime-main.75daa904.js"
+  },
+  {
+    "revision": "5da3f92f41ae8351126031df9a9cdddc",
+    "url": "/static/media/pexels-photo-669996.5da3f92f.webp"
+  }
+]);
